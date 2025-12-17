62d059817be37582e65c7a944e1a50b20c5fbda9
Nguyen Tri Cuong <nguyentricuong1504@gmail.com>
2025-12-17 10:06:33 +0700

    sửa nội dung thi cuối kỳ

--- src/App.js
+++ src/App.js
@@ -2,30 +2,36 @@ import './App.css';
 import { useEffect, useState } from 'react';
 
 function App() {
-  const [data, setData] = useState(null);
+ const [data, setData] = useState([]);
 
   useEffect(() => {
-    fetch('https://backend-tea-managerment-branch-service.onrender.com')
-      .then(response => {return response.json()})
-      .then(data => {
-        console.log("data::::",data); 
-        console.log(data);
-        setData(data);  // Lưu dữ liệu vào state
+     fetch("https://backend-tea-managerment-branch-service.onrender.com")
+      .then((response) => {
+        return response.json();
       })
-      .catch(error => console.error('Error fetching data:', error));
-  }, []); // [] để fetch 1 lần
-  // console.log("data::::",data);
-  
+      .then((data) => {
+        console.log("data::::", data);
+        console.log(data);
+        setData(data); // Lưu dữ liệu vào state
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+console.log(data)
   return (
     <div className="App">
       <h1>thực hành phần mềm mã nguồn mở</h1>
-      fetch data from backend: {data}
-      <h2>Nguyễn Trí Cường</h2>
-      <h2>MSSV: DH52200424</h2>
-      <h2>Lớp: D22-TH08</h2>
-      
+      {data.map((item) => {
+        return (
+          <div>
+            <h2>Nguyễn Trí Cường</h2>
+            <h2>MSSV: DH52200424</h2>
+            <h2>Lớp: D22-TH08</h2>
+            <h2> thứ 4 ca 2</h2>
+          </div>
+        );
+      })}
+            <h1>test lai</h1>
     </div>
   );
 }
-
 export default App;
